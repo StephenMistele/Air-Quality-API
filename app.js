ae256ee23045942ee8caa6aa2ec20db821122086
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-10-11 12:13:28 -0700

    Add gitignore?

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const bodyParser = require('body-parser');
+import fetch from "node-fetch";
+import express from "express";
+import bodyParser from "body-parser";
 const app = express()
 app.use(bodyParser.json());
 const port = 3000
@@ -15,6 +16,11 @@ app.post('/uploaduser', isAuthorized, (req,res) => {
     res.json([{
       status: 'Upload successful'
     }])
+})
+
+app.post('/getdata', isAuthorized, async (req,res) => {
+  let temp = await getAQIData(req.body);
+  res.send(temp);
 })
 
 function isAuthorized(req, res, next) {
@@ -38,6 +44,14 @@ function uploadData(body)
     let phone = body.phone;
     let email = body.email;
     return;
+}
+
+async function getAQIData(body)
+{
+  const response = await fetch("https://api.breezometer.com/air-quality/v2/current-conditions?lang=en&key=496bfcfb6ddd40ef831e29858c8ba7a9&metadata=extended_aqi&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,dominant_pollutant_concentrations,pollutants_concentrations,all_pollutants_concentrations,pollutants_aqi_information&lat=" + body.lat + "&lon=" + body.lon + "&all_aqi=true");
+  const res = await response.json();
+  delete res.metadata;
+  return res;
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`))

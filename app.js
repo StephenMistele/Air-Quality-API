3f523170d174db8988a1032e241db1172e9481b3
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-10-15 14:31:36 -0700

    Fix it nick

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const bodyParser = require("body-parser");
+const { json } = require("express");
 const express = require("express");
-const fetch = require("node-fetch");
+const request = require('request');
 const app = express();
 app.use(bodyParser.json());
 const port = 3000
 
 const accountSid = "ACb7936c6e692c39dcbc6619762a0e3050";
 const authToken = "4e9196ca16458ce98ae4fd1c80b0ba68";
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken); 
 
 
 app.get('/healthcheck', (req,res) => {
@@ -47,24 +48,35 @@ function isAuthorized(req, res, next) {
 
 function uploadData(body)
 {
-  /*formatting
-  name = body.name;
-  age = body.age;
-  weight = body.weight;
-  lat = body.lat;
-  lon = body.lon;
-  phone = body.phone;
-  email = body.email;
-  */
+  /*formatting*/
+  let name = body.name;
+  let age = body.age;
+  /*let weight = body.weight;
+  let lat = body.lat;
+  let lon = body.lon;
+  let phone = body.phone;
+  let email = body.email;*/
+  console.log(name, age)
   return;
 }
 
 async function getAQIData(body)
 {
-  const response = await fetch("https://api.breezometer.com/air-quality/v2/current-conditions?lang=en&key=496bfcfb6ddd40ef831e29858c8ba7a9&metadata=extended_aqi&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,dominant_pollutant_concentrations,pollutants_concentrations,all_pollutants_concentrations,pollutants_aqi_information&lat=" + body.lat + "&lon=" + body.lon + "&all_aqi=true");
-  const res = await response.json();
-  delete res.metadata;
-  return res;
+  let url = "https://api.breezometer.com/air-quality/v2/current-conditions?lang=en&key=496bfcfb6ddd40ef831e29858c8ba7a9&metadata=extended_aqi&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,dominant_pollutant_concentrations,pollutants_concentrations,all_pollutants_concentrations,pollutants_aqi_information&lat=" + body.lat + "&lon=" + body.lon + "&all_aqi=true";
+  try {
+    let response = await request(url, function (error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    }).body;
+    let res = JSON.parse(response);
+    delete res.metadata;
+    console.log(res);
+    return res; 
+  }
+  catch {
+    return "failed to query breezometer API";
+  }
+  
 }
 
 //driver function for notifying users regarding data. Called each morning
